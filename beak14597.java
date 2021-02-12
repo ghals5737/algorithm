ce20b05e4633338e193e1e4f62b1a9a106f8ad0a
ghals5737 <52785761+ghals5737@users.noreply.github.com>
2021-02-12 23:17:13 +0900

    Add files via upload
    20210212

---
+++ beak14597.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class beak14597 {
+	static int n,m;
+	static int[][] arr1,arr2;
+	static long dfs(int h,int a,int b,long sum) {
+		long num=(arr1[h][a]-arr2[h][b])*(arr1[h][a]-arr2[h][b]);
+		if(h==n-1) {
+			return sum;
+		}
+	    
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		n=Integer.parseInt(st.nextToken());
+		m=Integer.parseInt(st.nextToken());
+		arr1=new int[n][m];
+		arr2=new int[n][m];
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				arr1[i][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				arr2[i][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+		
+	}
+
+}
