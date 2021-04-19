d994e16376026beedc467a95e270295504899b3b
ghals5737 <52785761+ghals5737@users.noreply.github.com>
2021-04-19 21:57:41 +0900

    20210419

---
+++ leetcode1780.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean dfs(int n,int p){
+        if(n==0)return true;
+        int num=(int)Math.pow(3,p);
+        if(n>=num){
+            if(dfs(n-num,p+1))return true;
+            if(dfs(n,p+1))return true;
+        }
+        return false;
+    }
+    public boolean checkPowersOfThree(int n) {
+        return dfs(n,0);
+    }
+}
